80b11770d849b064806fa9a2452df5a9fd6e8fa1
Orumgbe <orumgbeugbekile@gmail.com>
2022-08-29 14:45:05 -0700

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,3 @@ def print_matrix_integer(matrix=[[]]):
                     print("{:d}".format(matrix[i][j]))
                 else:
                     print("{:d}".format(matrix[i][j]), end=" ")
-    else:
-        print()
