a5f006f8ca49b24decc08db34ad898b9d4c08d2a
Orumgbe <orumgbeugbekile@gmail.com>
2022-09-28 15:00:51 -0700

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Class that defines a square"""
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """initialise for every instance"""
         self.__size = size
         self.__position = position
@@ -60,24 +60,3 @@ class Square:
                 print(" " * right, end="")
                 print("#" * self.__size)
                 length -= 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
