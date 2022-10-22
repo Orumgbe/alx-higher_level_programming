0ad607389ec40913ae3b0043105ba529e6ea40d2
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-22 14:15:44 -0700

    fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,21 @@ class Rectangle:
         """Initialises instance attribute
             Args: width - Width of rectangle
                   height - Height of rectangle"""
-        self.__width = width
-        self.__height = height
+        if isinstance(width, int):
+            if (width >= 0):
+                self.__width = width
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+        if isinstance(height, int):
+            if (height >= 0):
+                self.__height = height
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
 
     @property
     def width(self):
@@ -23,7 +36,8 @@ class Rectangle:
         """Sets the value of width"""
         if not (isinstance(value, int)):
             raise TypeError("width must be an integer")
-        if value < 0:
+        else:
+            if value < 0:
                 raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -37,6 +51,7 @@ class Rectangle:
         """Sets the value of height"""
         if not (isinstance(value, int)):
             raise TypeError("height must be an integer")
-        if value < 0:
+        else:
+            if value < 0:
                 raise ValueError("height must be >= 0")
         self.__height = value
