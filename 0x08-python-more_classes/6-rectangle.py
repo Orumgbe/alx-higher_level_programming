564bebe1f4133bf2d40de8c08c892c75a04f6802
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-22 15:33:05 -0700

    Class attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
                             height (int)
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialises instance attribute
             Args: width - Width of rectangle
