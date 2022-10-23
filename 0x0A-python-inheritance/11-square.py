dd7c610bdff0e7531d403d9f56f89245710f64a6
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-23 12:42:42 -0700

    subclass of a subclass

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -21,4 +21,5 @@ class Square(Rectangle):
         return (self.__size**2)
 
     def __str__(self):
-        return ("[{}] {}/{}".format(type(self).__name__, self.__size, self.__size))
+        return ("[{}] {}/{}".format(type(self).__name__, self.__size,
+                                     self.__size))
