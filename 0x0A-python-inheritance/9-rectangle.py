dd6e6e19d9039fa6d2f02f11e89020dc7d18b1d0
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-23 12:13:32 -0700

    rectangle subclass

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,15 +10,14 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """initialises width and height"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
         self.__width = width
-        self.__heigth = height
+        self.integer_validator("height", height)
+        self.__height = height
 
-        def area(self):
-            """Returns area of the rectangle"""
-            return (self.__width * self.__height)
+    def area(self):
+        """Returns area of the rectangle"""
+        return (self.__width * self.__height)
 
-        def __str__(self):
-            """Returns string representation of object"""
-            return ("[{}] {}/{}".format(type(self).__name__, self.__width, self.__height))
+    def __str__(self):
+        """Returns string representation of object"""
+        return ("[{}] {}/{}".format(type(self).__name__, self.__width, self.__height))
