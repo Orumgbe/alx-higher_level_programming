d822c8b069883b2a26e4e8b5dc0a6b3a974bc496
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-24 10:16:50 -0700

    dictionary description of object, __dict__

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""8-class_to_json: class_to_json()"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure"""
+    return obj.__dict__
