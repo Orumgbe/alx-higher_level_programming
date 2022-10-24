0231de70f801d6cd53b83455097e9ed501d4ab95
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-24 08:06:32 -0700

    json serialization

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""3-to_json_string.py : to_json_string()"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns json serialized string"""
+    return (json.dumps(my_obj))
