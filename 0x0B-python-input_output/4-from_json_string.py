f51600d8a5876769f99ea76e7ce9c0f1b5641819
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-24 08:18:59 -0700

    json deserialization

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""4-from_json_string : from_json_string()"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns python object from json string"""
+    return (json.loads(my_str))
