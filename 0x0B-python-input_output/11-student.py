e877aa2b2594bf481f7283a0df3d49a9e88d4782
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-25 16:57:04 -0700

    set attribute of instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module : 10-student, Class : Student"""
+
+
+class Student:
+    """Defines a student
+        Attributes: first_name, last_name, age
+        Method: __init__(), to_json()
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialise attributes for every class instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of a Student instance
+            Args: attrs (list of attributes to retrieve in dictionary)"""
+        if attrs is not None:
+            my_d = {k: self.__dict__[k] for k in self.__dict__.keys() & attrs}
+            return my_d
+        else:
+            return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of a student instance
+            Args: json (a dictionary)
+                The dictionary key becomes the instance attribute
+                The dictionary key value becomes the value of the attribute"""
+        for k, v in json.items():
+            self.__setattr__(key, value)
