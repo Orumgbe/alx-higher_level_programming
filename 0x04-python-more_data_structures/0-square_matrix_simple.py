10620df7df9b08afa50bbdf2334a121fffc043ae
Orumgbe <orumgbeugbekile@gmail.com>
2023-03-02 06:22:24 -0800

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,9 +4,10 @@
 def square_matrix_simple(matrix=[]):
     """Computes square values of all integers of a matrix"""
     new_matrix = []
-    for i in range(len(matrix[0])):
-        nlist = []
+    nlist = []
+    for i in range(len(matrix)):
         for elem in matrix[i]:
             nlist.append(elem ** 2)
         new_matrix.append(nlist)
+        nlist = []
     return new_matrix
