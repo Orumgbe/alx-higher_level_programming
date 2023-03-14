f8fc92dd686d32fe909742cd88e44bc6e7165b63
Orumgbe <orumgbeugbekile@gmail.com>
2023-03-14 14:27:25 -0700

    task 5 to 9

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- prints full description of the table first_table from the database
+SHOW CREATE table first_table;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- counts how many records have id = 89;
+SELECT COUNT(id) FROM first_table
+WHERE id = 89;
