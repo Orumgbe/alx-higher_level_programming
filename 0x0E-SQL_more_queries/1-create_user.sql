0dd902bb7dbc6b1624d015652780f6d9eae7b5be
Orumgbe <orumgbeugbekile@gmail.com>
2023-03-15 15:04:30 -0700

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates new server user, name 'user_0d_1'
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
