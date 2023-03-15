23c6b67990115d96d2d7bd8e2fa5267a7d8b2d34
Orumgbe <orumgbeugbekile@gmail.com>
2023-03-15 15:34:14 -0700

    fix

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,3 @@
 -- Script creates table 'force_name'
-CREATE TABLE IF NOT EXISTS force_name(id INT, VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT,
+                                      name VARCHAR(256) NOT NULL);
