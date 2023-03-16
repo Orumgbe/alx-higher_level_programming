f499e2258afd76bcc3972e99fb7ecd988ee38485
Orumgbe <orumgbeugbekile@gmail.com>
2023-03-15 17:07:17 -0700

    fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- list all shows that have at least one genre linked
-SELECT ts.title AS title, tsg.genre_id as genre_id
+SELECT ts.title AS title, tsg.genre_id AS genre_id
 FROM tv_shows AS ts
 RIGHT JOIN tv_show_genres AS tsg
 ON ts.id = tsg.show_id
-ORDER BY title ASC, genre_id ASC;
+ORDER BY title, genre_id;
