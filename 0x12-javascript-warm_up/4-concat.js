07aa245aed3f2b5f13809f34c6c9788eef5b634b
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-13 06:47:43 -0700

    fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
 // Prints two arguments passed to it, format ... is ...
-if (process.argv[2] && process.argv[3]) {
-  let name = process.argv[2] + " is " + process.argv[3];
-  console.log(name);
-}
+let name = process.argv[2] + " is " + process.argv[3];
+console.log(name);
