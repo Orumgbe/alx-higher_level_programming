8e8b57182e2bdc28999d3a83f52a810cf8d40494
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-13 12:18:00 -0700

    second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Searches for the second biggest integer in list of arguments
+if (process.argv.length < 3) {
+  console.log('0');
+} else {
+  const args = [];
+
+  for (let i = 2; i < process.argv.length; i++) {
+    args[i - 2] = process.argv[i];
+  }
+  args.sort();
+  console.log(args[args.length - 2]);
+}
