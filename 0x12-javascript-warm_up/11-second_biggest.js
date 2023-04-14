6930444de7215b866015ef4d3e116957eedc1432
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-13 21:47:48 -0700

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 // Searches for the second biggest integer in list of arguments
-if (process.argv.length < 3) {
-  console.log('0');
+if (process.argv.length < 4) {
+  console.log(0);
 } else {
   const args = [];
 
