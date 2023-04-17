13c19c4e73b5e632276368d37a9d83daf3ff73bf
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-16 23:31:12 -0700

    fix requirements

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,4 +14,7 @@ if __name__ == '__main__':
     session = Session()
 
     first_state = session.query(State).first()
-    print("{}: {}".format(first_state.id, first_state.name))
+    if (first_state):
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
