fa82ebcb9fc7769bf3b47aed479ded98f352c6e3
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-16 23:52:26 -0700

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -6,7 +6,6 @@ from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                             sys.argv[1], sys.argv[2], sys.argv[3]))
@@ -19,3 +18,5 @@ if __name__ == '__main__':
         print("Not found")
     else:
         print(state.id)
+
+    session.close()
