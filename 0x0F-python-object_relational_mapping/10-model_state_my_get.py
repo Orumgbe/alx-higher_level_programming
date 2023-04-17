df06ffafdd3417951a8c25b870f104a80da219a1
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-16 23:44:21 -0700

    search state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Gets first state object stored in hbtn_0e_6_usa"""
+
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == "{}"\
+                                        .format(sys.argv[4])).first()
+    if (state):
+        print("{}".format(state.id))
+    else:
+        print("Not found")
