b0125ec32a07f14a8460a5e68a2904f527473e65
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-16 22:52:47 -0700

    first sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains states class definition that inherits from declarative_base()"""
+
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+class State(Base):
+    """Defines states table with columns 'id' and 'name'"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
