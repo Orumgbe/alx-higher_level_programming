c5deda9064711040e04a754a4ab87c2278e5ee08
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-16 22:54:49 -0700

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Contains states class definition that inherits from declarative_base()"""
 
-
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
+
 
 class State(Base):
     """Defines states table with columns 'id' and 'name'"""
