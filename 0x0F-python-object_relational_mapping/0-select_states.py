cf2787544ec12bdb43dfac2815ebd87eee0e4f28
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-17 17:42:08 -0700

    task 1 using MySQLdb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-"""
-A script that lists all states from the database hbtn_0e_0_usa
-"""
+"""This module connects to a locally hosted mysql server using MySQLdb
+    username, password and database name is passed to the terminal in order"""
+
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-            user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3],
-            host='localhost',
-            port=3306)
+    """module main function"""
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    dbname = sys.argv[3]
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             user=uname,
+                             passwd=pwd,
+                             db=dbname, port=3306)
+        cur = db.cursor()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [{}]: {}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: {}".format(e))
+        exit(1)
 
-    cursor = db.cursor()
+    try:
+        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [{}]: {}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: {}".format(e))
+        cur.close()
+        db.close()
+        exit(1)
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for item in rows:
+        print(item)
 
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+    cur.close()
+    db.close()
