15e5e47cd8613b2d5bfb8b9b7fe7c6cb64add6de
Orumgbe <orumgbeugbekile@gmail.com>
2023-07-25 17:20:08 -0700

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const movieID = process.argv[2];
-const link = `https://swapi-api.alx-tools.com/api/films/${movieID}`
+const link = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 request(link, function (err, response, body) {
   if (err) {
     return console.log(err);
