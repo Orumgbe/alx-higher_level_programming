10a472386f3a68af0c028f6e1cfe9bb55c97c6cc
Orumgbe <orumgbeugbekile@gmail.com>
2023-07-25 17:40:04 -0700

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const users = {};
+
+request(url, function (err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
+
+  const bodyObj = JSON.parse(body);
+
+  for (let i = 0; i < bodyObj.length; i++) {
+    const tmp = bodyObj[i].userId;
+    if (pBody[i].completed) {
+      users[tmp] = (users[tmp] || 0) + 1;
+    }
+  }
+  console.log(users);
+});
