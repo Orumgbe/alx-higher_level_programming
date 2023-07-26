17e1174475eb32711c623b55145f134cd5406f17
Orumgbe <orumgbeugbekile@gmail.com>
2023-07-25 17:18:53 -0700

    starwars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+const link = `https://swapi-api.alx-tools.com/api/films/${movieID}`
+request(link, function (err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(JSON.parse(body).title);
+});
