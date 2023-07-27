484ab65ff60099b2fec4aad53ec318b4553fc80d
Orumgbe <orumgbeugbekile@gmail.com>
2023-07-26 19:11:26 -0700

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send PUT request to passed url, only for user_id=98
-curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
