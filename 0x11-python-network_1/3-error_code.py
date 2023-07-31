15a9f4733a0f5a6286de6a9c51f21076bb519c0c
Orumgbe <orumgbeugbekile@gmail.com>
2023-07-31 09:17:46 -0700

    fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,10 +6,10 @@ from urllib import request, error
 
 
 if __name__ == "__main__":
-    req = request.Request(sys.argv[1])
+    url = sys.argv[1]
     try:
-        with request.urlopen(req) as resp:
+        with request.urlopen(url) as resp:
             html = resp.read().decode('utf-8')
-            print(html)
+            print("{}".format(html))
     except error.HTTPError as e:
         print("Error code: {}".format(e.code ))
